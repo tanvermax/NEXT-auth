ef87c18e222061a0c3b4ba7481c695090cf94acf
tanver909 <tanveermahidi@gmial.com>
2025-03-16 00:27:37 +0600

    next auth implmented succefully

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+const AboutPage=()=>{
+    return (
+        <div>
+      <h1 className="text-2xl font-bold text-center mt-5">it is about page</h1>
+
+            
+        </div>
+    )
+}
+export default AboutPage
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center mt-5">hello next js file</h1>
+    </div>
+  );
+}
